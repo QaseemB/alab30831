97ed4685b076d882a7bed6632a8e93f4307c61c0
QaseemB <Qbarnhardt@yahoo.com>
2024-05-23 20:52:41 -0400

    part 2

--- alab30831.js
+++ alab30831.js
@@ -8,21 +8,53 @@
 // If a number is not divisible by either 3 or 5, log the number.
 // Remember to commit your solution once it is working.
 
-let x = 1
+// let x = 1
 
-while (x <= 100){
-    // console.log(x)
-    if (x % 3 === 0 && x % 5 === 0){
-        console.log(x + " Fizz Buzz")
-    }else if(x % 3 === 0 ){
-        console.log(x + " Fizz")
-    } else{
-     if (x % 5 === 0){
-        console.log(x + " buzz")
-    }else {
-        console.log(x)
-    }
+// while (x <= 100){
+//     // console.log(x)
+//     if (x % 3 === 0 && x % 5 === 0){
+//         console.log(x + " Fizz Buzz")
+//     }else if(x % 3 === 0 ){
+//         console.log(x + " Fizz")
+//     } else{
+//      if (x % 5 === 0){
+//         console.log(x + " buzz")
+//     }else {
+//         console.log(x)
+//     }
 
-    }
-    x++
+//     }
+//     x++
+// }
+
+
+// Part 2: Prime Time
+// Now we will move onto something slightly more complex.
+// Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
+// Write a script that accomplishes the following:
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// As soon as you find the prime number, log that number and exit the loop.
+// Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
+// Be careful! If you set n to a number too large, your loop could take a long time to process.
+
+
+// for(n = 5; n <= 150; n++){
+//     // if(n % 2 !== 0 && n % 3 !== 0 && n % 5 !== 0){
+//     //     console.log(n)
+//     // }
+//     if( n !== 1)
+//     }
+
+
+let n = 2
+Primeloop: for (let i = n; i <100; i++){
+    if(n === 1) continue Primeloop;
+
+    for( let j = n; j>1; j--){
+        if( i % j === 0) continue Primeloop;
+      }
+    console.log(i)
+    break;
 }
+
